d0111e4d99279628c3b4ee32b99763a34641d25f
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-02-17 19:52:57 +0100

    InterestRateModel, VasicekModel and CoxIngersollRossModel classes added

--- interest_rates/int_rate_curve.py
+++ interest_rates/int_rate_curve.py
@@ -19,6 +19,7 @@
 #
 
 import time
+from abc import ABC, abstractmethod
 from multiprocessing import Pool
 import numpy as np
 import matplotlib.pyplot as plt
@@ -35,26 +36,21 @@ Z = lambda: np.random.normal()  # Standard normal r.v.
 dX = lambda: Z() * np.sqrt(dt)  # X(t) is a Brownian motion
 
 
-def integrate_r_dt(T: float) -> float:    
-    N = int(T / dt)
+def calc_approx_yield(T: float) -> float:
+    """
+    Calculate yield using an approximation of bond price using Monte Carlo simulation
+    :param T:
+    :return:
+    """
+    model: InterestRateModel = VasicekModel(
+        dt=0.01,
+        a=0.1,
+        b=0.07,
+        r0=0.02,
+        sigma=0.02,
+    )
 
-    r = np.zeros(N)
-    
-    r[0] = r0
-    
-    for k in range(N - 1):
-        # CIR model
-        #dr = a * (b - r[k]) * dt + sigma * np.sqrt(r[k]) * dX()
-        # Vasicek model
-        dr = a * (b - r[k]) * dt + sigma * dX()
-        r[k+1] = r[k]+dr
-        
-    integral = sum(r * dt)
-    return integral
-
-
-def bond_yield(T: float) -> float:
-    B = lambda: np.exp(-integrate_r_dt(T))
+    B = lambda: np.exp(-model.integrate_r_dt(T))
 
     BB = [B() for k in range(M)]
 
@@ -68,15 +64,93 @@ def bond_yield(T: float) -> float:
     
     
 
-def exact_bond_yield(T: float) -> float:
-    # Exact bond pricing formula for Vasicek model
+def calc_exact_yield(T: float) -> float:
+    """
+    # Calculate yield using exact bond pricing formula for Vasicek model
+    :param T:
+    :return:
+    """
     A = (1 - np.exp(-a * T)) / a
     B = (b - 0.5 * sigma**2 / a**2) * (A - T) - sigma**2 * A**2 / (4 * a)
     Z = np.exp(-A * r0 + B)
     y = - np.log(Z) / T
     
     return y
-    
+
+
+class InterestRateModel(ABC):
+    @abstractmethod
+    def integrate_r_dt(self, T: float) -> float:
+        ...
+
+
+class VasicekModel(InterestRateModel):
+    Z = lambda: np.random.normal()  # Standard normal r.v.
+    dX = lambda: Z() * np.sqrt(dt)  # X(t) is a Brownian motion
+
+    def __init__(
+            self,
+            dt: float,
+            a: float,
+            b: float,
+            r0: float,
+            sigma: float
+    ):
+        self.dt: float = dt
+        self.a: float = a
+        self.b: float = b
+        self.r0: float = r0
+        self.sigma: float = sigma
+
+    def integrate_r_dt(self, T: float) -> float:
+        N = int(T / self.dt)
+
+        r = np.zeros(N)
+
+        r[0] = self.r0
+
+        for k in range(N - 1):
+            # Vasicek model
+            dr = a * (b - r[k]) * dt + sigma * dX()
+            r[k + 1] = r[k] + dr
+
+        integral = sum(r * dt)
+        return integral
+
+
+class CoxIngersolRossModel(InterestRateModel):
+    Z = lambda: np.random.normal()  # Standard normal r.v.
+    dX = lambda: Z() * np.sqrt(dt)  # X(t) is a Brownian motion
+
+    def __init__(
+            self,
+            dt: float,
+            a: float,
+            b: float,
+            r0: float,
+            sigma: float
+    ):
+        self.dt: float = dt
+        self.a: float = a
+        self.b: float = b
+        self.r0: float = r0
+        self.sigma: float = sigma
+
+    def integrate_r_dt(self, T: float) -> float:
+        N = int(T / self.dt)
+
+        r = np.zeros(N)
+
+        r[0] = self.r0
+
+        for k in range(N - 1):
+            # CIR model
+            dr = a * (b - r[k]) * dt + sigma * np.sqrt(r[k]) * dX()
+            r[k + 1] = r[k] + dr
+
+        integral = sum(r * dt)
+        return integral
+
 
 if __name__ == '__main__':
     TT = np.arange(1, 10, 1)
@@ -85,7 +159,7 @@ if __name__ == '__main__':
 
     # Multi-core version    
     pool = Pool(6)
-    yy = pool.map(bond_yield, TT)
+    yy = pool.map(calc_approx_yield, TT)
     
     # Single-core version
     #yy = np.array(list(map(bond_yield, TT)))
@@ -94,7 +168,7 @@ if __name__ == '__main__':
     
     print('Time = ', tok-tik, 's')
     
-    yy2 = list(map(exact_bond_yield, TT))
+    yy2 = list(map(calc_exact_yield, TT))
 
     plt.plot(TT, yy)
     plt.plot(TT, yy2, 'k:')
