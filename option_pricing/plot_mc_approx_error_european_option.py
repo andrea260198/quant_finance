f5164e014d9677d23ee9aa42d81a9f5ceebb4535
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-04-21 15:51:39 +0200

    Multiprocessing added

--- option_pricing/plot_mc_approx_error_european_option.py
+++ option_pricing/plot_mc_approx_error_european_option.py
@@ -1,9 +1,10 @@
 from option_pricing.european_options import EuropeanCallOption
 import numpy as np
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 
-if __name__ == '__main__':
+def calc_std_error(M):
     option = EuropeanCallOption(
         T=10,
         r=0.20,
@@ -12,16 +13,18 @@ if __name__ == '__main__':
         K=100
     )
 
-    MM = [10, 100, 1000]#, 10_000, 100_000]
-    std_err_list = []
+    errors = np.array([option.price_mc_approx(M, 0.01) for k in range(100)]) - option.price_exact()
+    std_err = np.std(errors)
+    print(std_err)
+    return std_err
 
 
+if __name__ == '__main__':
+    MM = [10, 100, 1000, 10_000, 100_000]
 
-    for M in MM:
-        errors = np.array([option.price_mc_approx(M, 0.01) for k in range(100)]) - option.price_exact()
-        std_err = np.std(errors)
-        print(std_err)
-        std_err_list += [std_err]
+    pool = Pool(6)
+
+    std_err_list = list(pool.map(calc_std_error, MM))
 
     plt.loglog(MM, std_err_list, 'k')
     plt.ylabel('std err [$]')
