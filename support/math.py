a08bf276d20d109c69846e148d6373f194cae99d
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-04-27 12:20:40 +0200

    Support function to obtain the cartesian product added

---
+++ support/math.py
@@ -0,0 +1,12 @@
+from typing import TypeVar
+
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+
+
+def calc_cartesian_product(x: iter[T1], y: iter[T2]) -> list[list[tuple[T1, T2]]]:
+    I = len(x)
+    J = len(y)
+    X = [[(x[i], y[j]) for j in range(J)] for i in range(I)]
+    return X
