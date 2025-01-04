59a2ea908ee0f328b148f18bf41f7118d1922c5f
Andrea Cassotti <cassottiandrea@hotmail.com>
2025-01-04 18:19:57 +0100

    Pricing method enum added

--- option_pricing/abstract_option.py
+++ option_pricing/abstract_option.py
@@ -1,12 +1,26 @@
 from abc import ABC, abstractmethod
+from enum import Enum, auto
 from functools import cached_property
 from support.quant_dataclass import QuantDataclass
 
 
+class PricingMethod(Enum):
+    EXACT = auto()
+    APPROX = auto()
+
+
 class AbstractOption(QuantDataclass, ABC):
+    pricing_method: PricingMethod = PricingMethod.APPROX
+
     @cached_property
     def price(self) -> float:
-        return self.price_approx(100_000)
+        match self.pricing_method:
+            case PricingMethod.EXACT:
+                return self.price_exact()
+            case PricingMethod.APPROX:
+                return self.price_approx(100_000)
+            case _:
+                raise ValueError("Pricing method not defined.")
 
     @abstractmethod
     def price_approx(self, N: int) -> float:
