36af262039061e929367899cd869cabac1df0a7e
Andrea Cassotti <cassottiandrea@hotmail.com>
2025-01-22 23:01:06 +0100

    File renamed

---
+++ PricingEngine/tests/functional_tests/test_heston_option_pricing.py
@@ -0,0 +1,13 @@
+from implied_volatility.heston_model import HestonEuropeanCallOption
+
+
+def test_heston_european_call_option() -> None:
+    option = HestonEuropeanCallOption(
+        T=1,
+        r=0.20,
+        S_0=100,
+        sigma=0.20,
+        strike=100
+    )
+
+    price = option.price_mc_approx(1_000_000, 0.01)
