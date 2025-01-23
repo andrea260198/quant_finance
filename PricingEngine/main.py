3faa782043e1a99e4588006619bdc4df5f393892
Andrea Cassotti <cassottiandrea@hotmail.com>
2025-01-23 23:12:15 +0100

    Simple option pricing example added

--- PricingEngine/main.py
+++ PricingEngine/main.py
@@ -1,22 +1,30 @@
 from fastapi import FastAPI
 
-from option_pricing.european_barrier_options import EuropeanPutOptionBarrierOut
+from option_pricing.abstract_option import PricingMethod
+from option_pricing.european_options import EuropeanCallOption
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    option = EuropeanPutOptionBarrierOut(
-        r=0.05,
-        sigma=0.20,
-        S_0=100,
-        div=0.00,
-        T=1,
-        strike=100,
-        beta=0.5
-    )
+async def test():
+    return {"Hello": "World"}
 
-    price = option.price_approx(1000)
 
-    return {"Hello": price}
+@app.get("/price/{contract_name}")
+async def price_contract(contract_name: str):
+    print(contract_name)
+    match contract_name:
+        case "EuropeanCallOption":
+            option = EuropeanCallOption(
+                T=10,
+                r=0.05,
+                S_0=100,
+                sigma=0.20,
+                strike=100,
+                pricing_method=PricingMethod.EXACT
+            )
+            return {contract_name: option.price}
+        case _:
+            return {"Error": contract_name}
+
