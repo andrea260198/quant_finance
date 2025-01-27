a40c656a4ad1c987deae08554225e9c93a5cb7b1
Andrea Cassotti <cassottiandrea@hotmail.com>
2025-01-27 22:32:21 +0100

    Various pricing requests are satisfied

--- PricingEngine/main.py
+++ PricingEngine/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from implied_volatility.heston_model import HestonEuropeanCallOption
 from option_pricing.abstract_option import PricingMethod, AbstractOption
 from option_pricing.american_options import AmericanCallOption
 from option_pricing.european_options import EuropeanCallOption
@@ -15,11 +16,18 @@ async def test():
 
 
 @app.post("/contract/")
-async def price_contract(contract: Union[AmericanCallOption, EuropeanCallOption]):
+async def price_contract(contract: Union[EuropeanCallOption, AmericanCallOption, HestonEuropeanCallOption]) -> dict[str, float]:
+    print(type(contract))
     print(contract.type)
     match contract.type:
         case "EuropeanCallOption" as name:
             option = EuropeanCallOption.parse_raw(contract.json())
+            return {name: option.price}
+        case "AmericanCallOption" as name:
+            option = AmericanCallOption.parse_raw(contract.json())
+            return {name: option.price_approx(500)}
+        case "HestonEuropeanCallOption" as name:
+            option = HestonEuropeanCallOption.parse_raw(contract.json())
             return {name: option.price}
         case _:
             return {"Error": contract.type}
--- PricingEngine/send_request.py
+++ PricingEngine/send_request.py
@@ -1,5 +1,7 @@
 import requests
 
+from implied_volatility.heston_model import HestonEuropeanCallOption
+from option_pricing.american_options import AmericanCallOption
 from option_pricing.european_options import EuropeanCallOption
 
 
@@ -13,7 +15,25 @@ if __name__ == '__main__':
         sigma=0.20,
         strike=100
     ).dict()
-
     response = requests.post(url, json=data)
+    print(response.json())
 
+    data = AmericanCallOption(
+        T=10,
+        r=0.20,
+        S_0=100,
+        sigma=0.20,
+        strike=100
+    ).dict()
+    response = requests.post(url, json=data)
+    print(response.json())
+
+    data = HestonEuropeanCallOption(
+        T=10,
+        r=0.20,
+        S_0=100,
+        sigma=0.20,
+        strike=100
+    ).dict()
+    response = requests.post(url, json=data)
     print(response.json())
